ef4c0df77694a582b498edd58d43dabfb8f53c29
Stibsito <stibdiazc18@gmail.com>
2025-05-06 19:09:40 -0500

    idk

--- src/main/java/com/proyecto_lp2/repository/ICuentaRepository.java
+++ src/main/java/com/proyecto_lp2/repository/ICuentaRepository.java
@@ -1,5 +1,7 @@
 package com.proyecto_lp2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,13 +9,9 @@ import com.proyecto_lp2.model.Cuenta;
 
 @Repository
 public interface ICuentaRepository extends JpaRepository<Cuenta, Integer> {
-<<<<<<< HEAD
-	
-    Optional<Cuenta> findByEmail(String email);
-=======
 
 	Cuenta findByEmailAndClave(String email, String clave);
 
 	Cuenta findByEmail(String email);
->>>>>>> stib
+
 }
